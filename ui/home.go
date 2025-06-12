db4cd375e75825f8955684c1ef04b5c6f2f867ca
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-12 13:25:29 +0700

    feat: add clear terminal between pages

--- ui/home.go
+++ ui/home.go
@@ -16,6 +16,7 @@ func Home() {
 	println("1. Do conversion\n2. View conversion history\n0. Exit")
 	println("Please enter your choice (1-3):")
 	fmt.Scanf("%d", &choice)
+	utils.Clear()
 	switch choice {
 	case 1:
 		input()
@@ -25,15 +26,17 @@ func Home() {
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
 		if input == "" {
+			utils.Clear()
 			Home()
 		} else {
+			println("Invalid response.")
 			os.Exit(1)
 		}
 	case 0:
 		println("Exiting the program. Goodbye!")
 		os.Exit(0)
 	default:
-		println("Invalid choice. Please select a valid option.")
+		println("Invalid choice.")
 		os.Exit(1)
 	}
 }
