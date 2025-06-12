4ca1a73d428c10f8862ad537d160db107250c949
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-12 12:09:19 +0700

    feat: add convert program from celsius to other format

---
+++ utils/convert.go
@@ -0,0 +1,13 @@
+package utils
+
+func CtoK(c float64) float64 {
+	return c + 273.15
+}
+
+func CtoF(c float64) float64 {
+	return (c * 9 / 5) + 32
+}
+
+func CtoR(c float64) float64 {
+	return c * 4 / 5
+}
